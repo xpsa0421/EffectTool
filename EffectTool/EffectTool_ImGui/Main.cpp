8c5f3200e92499ac097b81de9fa91b6f6f1617d0
Sooa-Home <xpsa0421@connect.hku.hk>
2023-03-03 00:43:13 +0900

    ..

--- EffectTool/EffectTool_ImGui/Main.cpp
+++ EffectTool/EffectTool_ImGui/Main.cpp
@@ -1,7 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include "Std.h"
 #include "EffectTool.h"
 #include "imgui.h"
 #include "..\ImGui\imgui_internal.h"
+
 
 void ImGuiRender(EffectTool* effect_tool);
 
@@ -47,10 +49,42 @@ void ImGuiRender(EffectTool* effect_tool)
 	// Main menu bar
 	if (ImGui::BeginMainMenuBar())
 	{
-		ImGui::MenuItem("Save");
-		ImGui::MenuItem("Load");
+		// File menu
+		if (ImGui::BeginMenu("File"))
+		{
+			if (ImGui::MenuItem("New")) {}
+			if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+			if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+			if (ImGui::MenuItem("Save As..")) {}
+			ImGui::Separator();
+			if (ImGui::MenuItem("Quit", "Alt+F4")) 
+			{
+				effect_tool->game_active_ = false;
+			}
+			ImGui::EndMenu();
+		}
+
+		// Options menu
+		if (ImGui::BeginMenu("Options"))
+		{
+			ImGui::MenuItem("Render options", NULL, false, false);
+			static bool alpha_testing = true;
+			static bool alpha_blending = true;
+			ImGui::Checkbox("Alpha testing", &alpha_testing);
+			ImGui::Checkbox("Alpha blending", &alpha_blending);
+			ImGui::Separator();
+			static bool z_writing = false;
+			static bool z_comparing = true;
+			ImGui::Checkbox("Z-buffer writing", &z_writing);
+			ImGui::Checkbox("Z-buffer comparing", &z_comparing);
+			ImGui::Separator();
+			static bool wireframe = false;
+			ImGui::Checkbox("Wireframe", &wireframe);
+			ImGui::EndMenu();
+		}
 		ImGui::EndMainMenuBar();
 	}
+	
 
 	const ImGuiViewport* viewport = ImGui::GetMainViewport();
 	ImGui::SetNextWindowPos(viewport->WorkPos);
@@ -71,14 +105,15 @@ void ImGuiRender(EffectTool* effect_tool)
 		if (first_time)
 		{
 			first_time = false;
+
 			ImGui::DockBuilderRemoveNode(main_dockspace);
 			ImGui::DockBuilderAddNode(main_dockspace, ImGuiDockNodeFlags_DockSpace);
 			ImGui::DockBuilderSetNodeSize(main_dockspace, ImGui::GetMainViewport()->WorkSize);
 			
 			ImGuiID dock_id_left, dock_id_upLeft, dock_id_downLeft;
 			ImGuiID dock_id_right, dock_id_upRight, dock_id_downRight;
-			ImGui::DockBuilderSplitNode(main_dockspace, ImGuiDir_Left, 0.50f, &dock_id_left, &dock_id_right);
-			ImGui::DockBuilderSplitNode(dock_id_left, ImGuiDir_Up, 0.40f, &dock_id_upLeft, &dock_id_downLeft);
+			ImGui::DockBuilderSplitNode(main_dockspace, ImGuiDir_Left, 0.37f, &dock_id_left, &dock_id_right);
+			ImGui::DockBuilderSplitNode(dock_id_left, ImGuiDir_Up, 0.530f, &dock_id_upLeft, &dock_id_downLeft);
 			ImGui::DockBuilderSplitNode(dock_id_right, ImGuiDir_Up, 0.60f, &dock_id_upRight, &dock_id_downRight);
 
 			ImGui::DockBuilderDockWindow("Preview", dock_id_upLeft);
@@ -88,15 +123,13 @@ void ImGuiRender(EffectTool* effect_tool)
 
 			ImGui::DockBuilderFinish(main_dockspace);
 		}
+		ImGui::End();
 	}
-	ImGui::End();
-	ImGui::PopStyleVar();
-
-
+	
+	
 	window_flags = ImGuiWindowFlags_NoCollapse;
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
 
-	if (ImGui::Begin("Preview"))// , NULL, window_flags))
+	if (ImGui::Begin("Preview") , NULL, window_flags)
 	{
 		ImVec2 content_size = ImGui::GetContentRegionAvail();
 
@@ -104,21 +137,47 @@ void ImGuiRender(EffectTool* effect_tool)
 		effect_tool->ResizeViewport(content_size.x, content_size.y);
 		effect_tool->CoreRender();
 		ImGui::Image((void*)effect_tool->GetRenderedTexture(), content_size);
+		
+		ImGui::End();
 	}
-	ImGui::End();
-	ImGui::PopStyleVar();
+	
 
 
 	if (ImGui::Begin("Properties"))
 	{
-
+		ImGui::End();
 	}
-	ImGui::End();
+	
 
 
 	if (ImGui::Begin("Emitters"))
 	{
+		const int nrLayouts = 4;
+		const ImVec2 layoutSize{ 200, 0 };
+		const ImGuiWindowFlags layoutFlags = ImGuiWindowFlags_NoMove;
+		for (int i = 0; i < nrLayouts; i++) {
+			std::string layoutName{ std::string("Emitter##") + std::to_string(i) };
+			ImGui::PushStyleVar(ImGuiStyleVar_ChildBorderSize, 1.f);
+			if (ImGui::BeginChild(layoutName.c_str(), layoutSize, true)) {
 
+				ImGui::Text("Emitter %i", i);
+				//if (ImGui::TreeNode("Selection State: Single Selection"))
+				//{
+					static int selected = -1;
+					for (int n = 0; n < 5; n++)
+					{
+						char buf[32];
+						sprintf(buf, "Object %d", n);
+						if (ImGui::Selectable(buf, selected == n))
+							selected = n;
+					}
+					//ImGui::TreePop();
+				//}
+			}
+			ImGui::PopStyleVar();
+			ImGui::EndChild();
+			ImGui::SameLine(0,0);
+		}
 	}
 	ImGui::End();
 
@@ -127,4 +186,5 @@ void ImGuiRender(EffectTool* effect_tool)
 
 	}
 	ImGui::End();
+	ImGui::PopStyleVar();
 }
