5ae728a60550dcde074b6f3b7136b832fe864b4b
Sooa-kgca <xpsa0421@connect.hku.hk>
2023-03-06 15:46:49 +0900

    created tabs for particle system

--- EffectTool/EffectTool_ImGui/Main.cpp
+++ EffectTool/EffectTool_ImGui/Main.cpp
@@ -7,7 +7,8 @@
 //------------------------------------------------------------
 // Forward declarations of imgui render functions
 void ImGuiRender(EffectTool* effect_tool);
-void CreateParticleViewer(EffectTool* effect_tool, int particle_idx);
+void ImGuiCreateMainMenu(EffectTool* effect_tool);
+void ImGuiCreatePSWindow(EffectTool* effect_tool, int particle_idx);
 //------------------------------------------------------------
 
 
@@ -32,7 +33,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		if (effect_tool.window_->Run() == true)
 		{
 			effect_tool.CoreFrame();
-			effect_tool.CoreRender();
 
 			// Render
 			ImGuiLayer::Begin();
@@ -49,7 +49,50 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	effect_tool.CoreRelease();
 }
 
+
+
 void ImGuiRender(EffectTool* effect_tool)
+{
+	ImGuiCreateMainMenu(effect_tool);
+
+	//------------------------------------------------------------------------------------------
+	// main docking window
+	const ImGuiViewport* viewport = ImGui::GetMainViewport();
+	ImGui::SetNextWindowPos(viewport->WorkPos);
+	ImGui::SetNextWindowSize(viewport->WorkSize);
+	ImGui::SetNextWindowViewport(viewport->ID);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBackground;
+	window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoDocking;
+	ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+	
+	ImGui::Begin("Master Window", nullptr, window_flags);
+	ImGui::PopStyleVar(3);
+	ImGuiID master_dockspace_id = ImGui::GetID("Master DockSpace");
+	ImGui::DockSpace(master_dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+	ImGui::End();
+	//------------------------------------------------------------------------------------------
+
+	//------------------------------------------------------------------------------------------
+	// emitter 1 window
+	ImGui::SetNextWindowDockID(master_dockspace_id, ImGuiCond_Once);
+	ImGuiCreatePSWindow(effect_tool, 0);
+
+
+	//------------------------------------------------------------------------------------------
+	ImGui::SetNextWindowDockID(master_dockspace_id, ImGuiCond_Once);
+	if (ImGui::Begin("ParticleSystem2"))
+	{
+		ImGui::TextUnformatted("Test 2");
+	}
+	ImGui::End();
+
+}
+
+
+void ImGuiCreateMainMenu(EffectTool* effect_tool)
 {
 	// Main menu bar
 	if (ImGui::BeginMainMenuBar())
@@ -89,140 +132,98 @@ void ImGuiRender(EffectTool* effect_tool)
 		}
 		ImGui::EndMainMenuBar();
 	}
-
-	/*const ImGuiViewport* viewport = ImGui::GetMainViewport();
-	ImGui::SetNextWindowPos(viewport->WorkPos);
-	ImGui::SetNextWindowSize(viewport->WorkSize);
-	ImGui::SetNextWindowViewport(viewport->ID);
-	ImGuiWindowFlags host_window_flags = 0;
-	host_window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoDocking;
-	host_window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoBackground;
-
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
-	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-	ImGui::Begin("HostWindow", NULL, host_window_flags);
-	ImGui::PopStyleVar(3);
-	ImGuiID dockspace_id = ImGui::GetID("HostDockSpace");
-	ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
-	ImGui::End();*/
-
-
-	//ImGui::Begin("HostWindow");
-		/*ImGuiID host_dockspace_id = ImGui::GetID("HostDockspace"); 
-		ImGui::DockSpace(host_dockspace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode | ImGuiDockNodeFlags_NoResize);*/
-		ImGuiID host_dockspace_id = ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), ImGuiDockNodeFlags_PassthruCentralNode);
-	//ImGui::End();
-
-	ImGui::SetNextWindowDockID(host_dockspace_id, ImGuiCond_FirstUseEver);
-	if (ImGui::Begin("Window1"), ImGuiWindowFlags_NoBackground) { ImGui::Text("window 1"); }
-	ImGui::End();
-
-	ImGui::SetNextWindowDockID(host_dockspace_id, ImGuiCond_FirstUseEver);
-	if (ImGui::Begin("Window2")) { ImGui::Text("window 2"); }
-	ImGui::End();
-
-	//CreateParticleViewer(effect_tool, 0);
 }
 
-static void CreateParticleViewer(EffectTool* effect_tool, int particle_idx)
+void ImGuiCreatePSWindow(EffectTool* effect_tool, int ps_idx)
 {
-	//const ImGuiViewport* viewport = ImGui::GetMainViewport();
-	//ImGui::SetNextWindowPos(viewport->WorkPos);
-	//ImGui::SetNextWindowSize(viewport->WorkSize);
-	//ImGui::SetNextWindowViewport(viewport->ID);
-	//ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize /*| ImGuiWindowFlags_NoMove*/;
-	//window_flags |= /*ImGuiWindowFlags_NoBringToFrontOnFocus | */ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoDocking;
-	//ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoMove;
 
-	//ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
-	//if (ImGui::Begin("Particle##" + particle_idx, NULL, window_flags))
-	//{
-	//	ImGuiID main_dockspace = ImGui::GetID("MyDockSpace##" + particle_idx);
-	//	ImGui::DockSpace(main_dockspace, ImVec2(0.0f, 0.0f), dockspace_flags);
+	ImGui::Begin("PS Window##" + ps_idx, NULL, window_flags);
+	ImVec2 emitter_content_size = ImGui::GetContentRegionAvail();
+	ImGuiID main_dockspace = ImGui::GetID("PS DockSpace##" + ps_idx);
+	ImGui::DockSpace(main_dockspace, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
 
-	//	static auto first_time = true;
-	//	if (first_time)
-	//	{
-	//		first_time = false;
+	static auto first_time = true;
+	if (first_time)
+	{
+		first_time = false;
 
-	//		ImGui::DockBuilderRemoveNode(main_dockspace);
-	//		ImGui::DockBuilderAddNode(main_dockspace, ImGuiDockNodeFlags_DockSpace);
-	//		ImGui::DockBuilderSetNodeSize(main_dockspace, ImGui::GetMainViewport()->WorkSize);
+		ImGui::DockBuilderRemoveNode(main_dockspace);
+		ImGui::DockBuilderAddNode(main_dockspace, ImGuiDockNodeFlags_DockSpace);
+		ImGui::DockBuilderSetNodeSize(main_dockspace, emitter_content_size);
 
-	//		ImGuiID dock_id_left, dock_id_upLeft, dock_id_downLeft;
-	//		ImGuiID dock_id_right, dock_id_upRight, dock_id_downRight;
-	//		ImGui::DockBuilderSplitNode(main_dockspace, ImGuiDir_Left, 0.37f, &dock_id_left, &dock_id_right);
-	//		ImGui::DockBuilderSplitNode(dock_id_left, ImGuiDir_Up, 0.530f, &dock_id_upLeft, &dock_id_downLeft);
-	//		ImGui::DockBuilderSplitNode(dock_id_right, ImGuiDir_Up, 0.60f, &dock_id_upRight, &dock_id_downRight);
+		ImGuiID dock_id_left, dock_id_upLeft, dock_id_downLeft;
+		ImGuiID dock_id_right, dock_id_upRight, dock_id_downRight;
+		ImGui::DockBuilderSplitNode(main_dockspace, ImGuiDir_Left, 0.37f, &dock_id_left, &dock_id_right);
+		ImGui::DockBuilderSplitNode(dock_id_left, ImGuiDir_Up, 0.530f, &dock_id_upLeft, &dock_id_downLeft);
+		ImGui::DockBuilderSplitNode(dock_id_right, ImGuiDir_Up, 0.60f, &dock_id_upRight, &dock_id_downRight);
 
-	//		ImGui::DockBuilderDockWindow("Preview##" + particle_idx, dock_id_upLeft);
-	//		ImGui::DockBuilderDockWindow("Properties##" + particle_idx, dock_id_downLeft);
-	//		ImGui::DockBuilderDockWindow("Emitters##" + particle_idx, dock_id_upRight);
-	//		ImGui::DockBuilderDockWindow("Curve Editor##" + particle_idx, dock_id_downRight);
+		ImGui::DockBuilderDockWindow("Preview##" + ps_idx, dock_id_upLeft);
+		ImGui::DockBuilderDockWindow("Properties##" + ps_idx, dock_id_downLeft);
+		ImGui::DockBuilderDockWindow("Emitters##" + ps_idx, dock_id_upRight);
+		ImGui::DockBuilderDockWindow("Curve Editor##" + ps_idx, dock_id_downRight);
 
-	//		ImGui::DockBuilderFinish(main_dockspace);
-	//	}
-	//}
-	//ImGui::End();
+		ImGui::DockBuilderFinish(main_dockspace);
+	}
+	ImGui::End();
 
-	//window_flags = ImGuiWindowFlags_NoCollapse;
+	window_flags = ImGuiWindowFlags_NoCollapse;
 
-	//if (ImGui::Begin("Preview##" + particle_idx), NULL, window_flags)
-	//{
-	//	ImVec2 content_size = ImGui::GetContentRegionAvail();
+	if (ImGui::Begin("Preview##" + ps_idx), NULL, window_flags)
+	{
+		ImVec2 content_size = ImGui::GetContentRegionAvail();
 
-	//	// Render mainframe
-	//	effect_tool->ResizeViewport(content_size.x, content_size.y);
-	//	effect_tool->CoreRender();
-	//	ImGui::Image((void*)effect_tool->GetRenderedTexture(), content_size);
+		// Render mainframe
+		effect_tool->ResizeViewport(content_size.x, content_size.y);
+		effect_tool->CoreRender();
+		ImGui::Image((void*)effect_tool->GetRenderedTexture(), content_size);
 
-	//	ImGui::End();
-	//}
+		ImGui::End();
+	}
 
-	//window_flags = ImGuiWindowFlags_HorizontalScrollbar;
-	//const int numEmitters = 4;
-	//ImGui::SetNextWindowContentSize(ImVec2(numEmitters * 200 + 5, 0));
+	window_flags = ImGuiWindowFlags_HorizontalScrollbar;
+	int numEmitters = 5;
+	ImGui::SetNextWindowContentSize(ImVec2(numEmitters * 200 + 5, 0));
 
-	//static bool display_emitters = true;
-	//if (ImGui::Begin("Emitters##" + particle_idx, &display_emitters, ImGuiWindowFlags_HorizontalScrollbar))
-	//{
-	//	int numProps = 5;
-	//	static std::vector<int> selected(numProps, -1);
-	//	const ImVec2 layoutSize{ 200, 0 };
-	//	const ImGuiWindowFlags layoutFlags = ImGuiWindowFlags_NoMove;
+	static bool display_emitters = true;
+	if (ImGui::Begin("Emitters##" + ps_idx, &display_emitters, ImGuiWindowFlags_HorizontalScrollbar))
+	{
+		int numProps = 5;
+		static std::vector<int> selected(numProps, -1);
+		const ImVec2 layoutSize{ 200, 0 };
+		const ImGuiWindowFlags layoutFlags = ImGuiWindowFlags_NoMove;
 
-	//	for (int emitter_idx = 0; emitter_idx < numEmitters; emitter_idx++) {
-	//		std::string layoutName{ std::string("Emitter##" + particle_idx) + std::to_string(emitter_idx) };
-	//		ImGui::PushStyleVar(ImGuiStyleVar_ChildBorderSize, 1.f);
+		for (int emitter_idx = 0; emitter_idx < numEmitters; emitter_idx++) {
+			std::string layoutName{ std::string("Emitter ") + std::to_string(emitter_idx) + "##" + std::to_string(ps_idx) };
+			ImGui::PushStyleVar(ImGuiStyleVar_ChildBorderSize, 1.f);
 
-	//		if (ImGui::BeginChild(layoutName.c_str(), layoutSize, true)) {
-	//			ImGui::Text("Emitter %i", emitter_idx);
-	//			for (int prop_idx = 0; prop_idx < numProps; prop_idx++)
-	//			{
-	//				std::string name = "Property##" + particle_idx + std::to_string(prop_idx);
-	//				if (ImGui::Selectable(name.c_str(), selected[emitter_idx] == prop_idx))
-	//					selected[emitter_idx] = prop_idx;
-	//			}
-	//		}
+			if (ImGui::BeginChild(layoutName.c_str(), layoutSize, true)) {
+				ImGui::Text("Emitter %i", emitter_idx);
+				for (int prop_idx = 0; prop_idx < numProps; prop_idx++)
+				{
+					std::string name = "Property " + std::to_string(prop_idx) + "##" + std::to_string(ps_idx);
+					if (ImGui::Selectable(name.c_str(), selected[emitter_idx] == prop_idx))
+						selected[emitter_idx] = prop_idx;
+				}
+			}
+			ImGui::PopStyleVar();
+			ImGui::EndChild();
+			ImGui::SameLine(0, 0);
+		}
+	}
+	ImGui::End();
 
-	//		ImGui::PopStyleVar();
-	//		ImGui::EndChild();
-	//		ImGui::SameLine(0, 0);
-	//	}
-	//}
-	//ImGui::End();
+	window_flags = ImGuiWindowFlags_NoCollapse;
+	if (ImGui::Begin("Properties##" + ps_idx))
+	{
 
-	//window_flags &= !ImGuiWindowFlags_HorizontalScrollbar;
-	//if (ImGui::Begin("Properties##" + particle_idx))
-	//{
+	}ImGui::End();
 
-	//}ImGui::End();
+	if (ImGui::Begin("Curve Editor##" + ps_idx))
+	{
 
-	//if (ImGui::Begin("Curve Editor##" + particle_idx))
-	//{
-
-	//}
-	//ImGui::End();
-	//ImGui::PopStyleVar();
+	}
+	ImGui::End();
+	ImGui::PopStyleVar();
 }
